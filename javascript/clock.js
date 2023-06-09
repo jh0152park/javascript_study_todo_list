9b2ca2a8fc096ac78d0581232e182c62b759ccc4
jh0152park <jh0152park@naver.com>
2023-06-09 21:42:09 +0900

    changed 3.png background image file

--- javascript/clock.js
+++ javascript/clock.js
@@ -4,7 +4,19 @@ function updateCurrentTime() {
   const min = date.getMinutes().toString().padStart(2, "0");
   const sec = date.getSeconds().toString().padStart(2, "0");
   const clock = document.querySelector("#clock");
+
   clock.innerText = `${hour}:${min}:${sec}`;
 }
 
+function updateCurrentDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const today = document.querySelector("#date");
+
+  today.innerText = `${year}/${month}/${day}`;
+}
+
 setInterval(updateCurrentTime, 1000);
+setInterval(updateCurrentDate, 1000);
