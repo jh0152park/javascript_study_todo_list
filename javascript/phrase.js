be4cab8b4bb3171fef01b5ea92e1209c87b3d399
jh0152park <jh0152park@naver.com>
2023-06-09 23:04:49 +0900

    added color animation at realtime clock

---
+++ javascript/phrase.js
@@ -0,0 +1,42 @@
+const lines = [
+  {
+    line: "When you believe in a thing, believe in it all the way, implicitly and unquestionable.",
+    speaker: "Walt Disney",
+  },
+  {
+    line: "Never say goodbye because goodbye means going away and going away means forgetting.",
+    speaker: " Peter Pan",
+  },
+  {
+    line: "Some people are worth melting for.",
+    speaker: "Frozen(Olaf)",
+  },
+  {
+    line: "The flower that blooms in adversity is the most rare and beautiful of all.",
+    speaker: "Mulan",
+  },
+  {
+    line: "If you focus on what you left behind, you will never see what lies ahead.",
+    speaker: "Ratatouille",
+  },
+  {
+    line: "The past can hurt. But from the way I see it, you can either run from it, or learn from it.",
+    speaker: "Lion King",
+  },
+  {
+    line: "You cannot be happy every day. But there are happy things every day.",
+    speaker: "Winnie the Pooh",
+  },
+  {
+    line: "To love someone, you have to love yourself first.",
+    speaker: "Beauty and the Beast",
+  },
+  {
+    line: "Share your gift with the world. Who knows how many lives you are saving just by doing so.",
+    speaker: "Kung Fu Panda",
+  },
+  {
+    line: "Venture outside your comfort zone. The rewards are worth it.",
+    speaker: "Rapunzel",
+  },
+];
